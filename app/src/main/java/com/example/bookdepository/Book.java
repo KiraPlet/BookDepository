70ba549aefbba04212cdc3f1f3e571b0d80fa37f
Your Name <you@example.com>
2025-04-15 13:50:56 +0300

    седьмой пункт в методички

--- app/src/main/java/com/example/bookdepository/Book.java
+++ app/src/main/java/com/example/bookdepository/Book.java
@@ -1,12 +1,18 @@
 package com.example.bookdepository;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Book {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mReaded;
+
+
     public Book() {
         mId = UUID.randomUUID(); //Генерирование уникального идентификатора
+        mDate = new Date();
     }
     public UUID getId() {
         return mId;
@@ -16,5 +22,20 @@ public class Book {
     }
     public void setTitle(String title) {
         mTitle = title;
+    }
+    public Date getDate(){
+
+        return mDate;
+    }
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    public boolean isReaded() {
+
+        return mReaded;
+    }
+    public void setReaded(boolean readed) {
+
+        mReaded = readed;
     }
 }
