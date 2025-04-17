c05a86307f802c36c7ae0d844c2cfd04bc963b96
Your Name <you@example.com>
2025-04-17 11:40:53 +0300

    Объектно-ориентированный подход в разработке кода информационной системы

---
+++ app/src/main/java/com/example/bookdepository/BookListActivity.java
@@ -0,0 +1,12 @@
+package com.example.bookdepository;
+
+
+import androidx.fragment.app.Fragment;
+
+public class BookListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment(){
+        return new BookListFragment();
+    }
+}
